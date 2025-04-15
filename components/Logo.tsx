3a1cfaf0c2146bcc1b6a6e9c43177c42838b795e
coderhs <mailme@hsps.in>
2025-04-15 13:40:01 +0530

    Link to home from app logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,6 +2,7 @@
 
 import { Box } from '@mantine/core';
 import { useMantineColorScheme } from '@mantine/core';
+import Link from 'next/link';
 
 export default function Logo() {
   const { colorScheme } = useMantineColorScheme();
@@ -9,16 +10,17 @@ export default function Logo() {
   const logoUrl =
     colorScheme === 'dark' ? '/wifi-qr-white.png' : '/wifi-qr-logo.png';
 
-
   return (
-    <Box
-      style={{
-        backgroundImage: `url(${logoUrl})`,
-        backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        width: 240,
-        height: 80,
-      }}
-    />
+    <Link href="/" passHref>
+      <Box
+        style={{
+          backgroundImage: `url(${logoUrl})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          width: 240,
+          height: 80,
+        }}
+      />
+    </Link>
   )
 }
